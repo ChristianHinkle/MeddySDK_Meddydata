40edfcf2937e6857f6c9e165058a257a6e15c1a8
Christian Hinkle <christianhinko@gmail.com>
2026-03-27 12:57:34 -0600

    [files][rename][cpp][cmake] Rename "Meddydata" source files to "Utils".

--- Source/Files/Utils.cpp
+++ Source/Files/Utils.cpp
@@ -1,11 +1,12 @@
 // Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
 
-#include <MeddySDK/Meddydata/Meddydata.h>
+#include <MeddySDK/Meddydata/Utils.h>
 
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <cstdlib>
 #include <cassert>
+#include <MeddySDK/Meddyproject/Utils.h>
 
 boost::filesystem::path MeddySDK::GetPathToMeddydata(
     boost::filesystem::path&& sourceFilePath)
